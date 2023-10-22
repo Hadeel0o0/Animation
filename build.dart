e08bc98ed2eadc2990f625a617bd9e986571b15b
Hadeel0o0 <111194916+Hadeel0o0@users.noreply.github.com>
2023-10-22 23:43:16 +0300

    Add files via upload

---
+++ build.dart
@@ -0,0 +1,46 @@
+import 'dart:math' as math;
+import 'package:flutter/material.dart';
+
+/// Flutter code sample for [AnimatedBuilder].
+
+class AnimatedBuilderExample extends StatefulWidget {
+  const AnimatedBuilderExample({super.key});
+
+  @override
+  State<AnimatedBuilderExample> createState() => _AnimatedBuilderExampleState();
+}
+
+class _AnimatedBuilderExampleState extends State<AnimatedBuilderExample>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 10),
+    vsync: this,
+  )..repeat();
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigo[500],
+      body: AnimatedBuilder(
+        animation: _controller,
+        child: const Center(
+          child: FlutterLogo(
+            size: 100,
+          ),
+        ),
+        builder: (BuildContext context, Widget? child) {
+          return Transform.rotate(
+            angle: _controller.value * 2.0 * math.pi,
+            child: child,
+          );
+        },
+      ),
+    );
+  }
+}
